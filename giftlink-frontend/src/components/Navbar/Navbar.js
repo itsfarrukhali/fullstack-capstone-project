999c7831d7ed814a2ce9b8e2df581e952dde2d3e
itsfarrukhali <afarrukh302@gmail.com>
2025-12-17 15:07:35 -0500

    Completed Landing Page and MainPage with Navbar

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -1,15 +1,98 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="/">GiftLink</a>
+export default function Navbar({ isLoggedIn, handleLogout }) {
+  const navigate = useNavigate();
 
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    {/* Task 1: Add links to Home and Gifts below*/}
-                </ul>
-            </div>
-        </nav>
-    );
+  const goToProfile = () => {
+    navigate("/app/profile");
+  };
+
+  const goToLogin = () => {
+    navigate("/app/login");
+  };
+
+  const goToRegister = () => {
+    navigate("/app/register");
+  };
+
+  const goToSearch = () => {
+    navigate("/app/search");
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">
+          GiftLink
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            {/* Task 1: Add links to Home and Gifts */}
+            <li className="nav-item">
+              <a className="nav-link" href="/home.html">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/app">
+                Gifts
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <button className="nav-link btn btn-link" onClick={goToSearch}>
+                Search
+              </button>
+            </li>
+
+            {isLoggedIn ? (
+              <>
+                <li className="nav-item">
+                  <button
+                    className="nav-link btn btn-link"
+                    onClick={goToProfile}
+                  >
+                    Profile
+                  </button>
+                </li>
+                <li className="nav-item">
+                  <button
+                    className="nav-link btn btn-link"
+                    onClick={handleLogout}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-item">
+                  <button className="nav-link btn btn-link" onClick={goToLogin}>
+                    Login
+                  </button>
+                </li>
+                <li className="nav-item">
+                  <button
+                    className="nav-link btn btn-link"
+                    onClick={goToRegister}
+                  >
+                    Register
+                  </button>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
